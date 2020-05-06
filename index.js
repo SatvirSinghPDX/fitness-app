6f50a396e4036b63b8877f8f03b3485c914d138b
Satvir Singh <satvir@pdx.edu>
2020-05-06 16:54:33 -0700

    updated navigation bar for sign in button

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out.');
-        document.getElementById("Username").innerHTML = 'User signed out';
-        document.getElementById("Username").style.color = "red";
+       /*  document.getElementById("Username").innerHTML = 'User signed out';
+        document.getElementById("Username").style.color = "red"; */
     });
 }
 fetch(CategoryUrl)
