febd6b561f95eaac7339f6f9841fbea251e42ddb
Satvir Singh <satvir@pdx.edu>
2020-05-05 20:21:06 -0700

    added sign in and sign out capability

--- index.js
+++ index.js
@@ -3,6 +3,41 @@ const EquipmentUrl = 'https://wger.de/api/v2/equipment';
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
 // Enter your code here
+function onSuccess(googleUser) {
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+    /* document.getElementById("imageField").src = googleUser.getBasicProfile().getImageUrl(); */
+    document.getElementById("Username").innerHTML = googleUser.getBasicProfile().getName();
+    document.getElementById("Username").style.color = "red";
+}
+function onFailure(error) {
+    console.log(error);
+}
+function renderButton() {
+    gapi.signin2.render('my-signin2', {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+    });
+}
+/* function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+} */
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+        document.getElementById("Username").innerHTML = 'User signed out';
+        document.getElementById("Username").style.color = "red";
+    });
+}
 fetch(CategoryUrl)
     .then(
         function (response) {
