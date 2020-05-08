05bf918fdde753204da8c00b6486115d751f7dc8
Satvir Singh <satvir@pdx.edu>
2020-05-08 16:28:02 -0700

    cards now pop up with health related info when the image of the food is clicked

--- food.js
+++ food.js
@@ -1,4 +1,4 @@
-//const url = 'https://api.spoonacular.com/recipes/random?number=20&tags=vegetarian&apiKey=ed5d36b12a2448308cb411d99ccc2a06';
+const url = 'https://api.spoonacular.com/recipes/random?number=20&tags=vegetarian&apiKey=ed5d36b12a2448308cb411d99ccc2a06';
 
 fetch(url)
     .then(
@@ -11,7 +11,9 @@ fetch(url)
                 for (i = 0; i < 20; i++) {
                     console.log(data.recipes[i].title);
                     console.log(data.recipes[i].image);
-                    document.getElementById("foodCardDiv").innerHTML += "<div id='foodCards' class='card'><img id='foodCardImg' class='card-img-top' src='" + data.recipes[i].image + "' alt='Card image cap'><div class='card-body'><a id='foodName' class='card-text' href='" + data.recipes[i].spoonacularSourceUrl + "'>" + data.recipes[i].title + "</a></div></div>";
+                    console.log(data.recipes[i].healthScore);
+                    document.getElementById("foodCardDiv").innerHTML += "<div id='foodCards' class='card'><img  data-toggle='modal' data-target='#exampleModalCenter' id='foodCardImg' class='card-img-top' src='" + data.recipes[i].image + "' alt='Card image cap'><div class='card-body'><a id='foodName' class='card-text' href='" + data.recipes[i].spoonacularSourceUrl + "'>" + data.recipes[i].title + "</a></div></div>";
+                    document.getElementById("popupBody").innerHTML = "Health Score: " + data.recipes[i].healthScore + "<br /> Dairy Free: " + data.recipes[i].dairyFree + "<br /> Gluten Free: " + data.recipes[i].glutenFree + "<br /> Vegan: " + data.recipes[i].vegan + "<br /> Low Fodmap: " + data.recipes[i].lowFodmap;
                 }
             });
         }
