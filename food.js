028502b432ee2d97f90c6167197c178484a07c9d
Satvir Singh <satvir@pdx.edu>
2020-05-08 18:37:50 -0700

    changed true/false fields to yes/no for food information

--- food.js
+++ food.js
@@ -12,9 +12,30 @@ fetch(url)
                     console.log(data.recipes[i].title);
                     console.log(data.recipes[i].image);
                     console.log(data.recipes[i].healthScore);
+                    var healthScore = data.recipes[i].healthScore;
+                    var dairyFree = data.recipes[i].dairyFree;
+                    if (dairyFree == true)
+                        dairyFree = "Yes";
+                    else
+                        dairyFree = "No";
+                    var glutenFree = data.recipes[i].glutenFree;
+                    if (glutenFree == true)
+                        glutenFree = "Yes";
+                    else
+                        glutenFree = "No";
+                    var vegan = data.recipes[i].vegan;
+                    if (vegan == true)
+                        vegan = "Yes";
+                    else
+                        vegan = "No";
+                    var lowFodmap = data.recipes[i].lowFodmap;
+                    if (lowFodmap == true)
+                        lowFodmap = "Yes";
+                    else
+                        lowFodmap = "No";
                     document.getElementById("foodCardDiv").innerHTML += "<div id='foodCards' class='card'><img  data-toggle='modal' data-target='#exampleModalCenter" + i + "' id='foodCardImg' class='card-img-top' src='" + data.recipes[i].image + "' alt='Card image cap'><div class='card-body'><a id='foodName' class='card-text' href='" + data.recipes[i].spoonacularSourceUrl + "'>" + data.recipes[i].title + "</a></div></div>";
                     document.getElementById("popupBox").innerHTML += "<div class='modal fade' id='exampleModalCenter" + i + "' tabindex='-1' role='dialog' aria-labelledby='exampleModalCenterTitle' aria-hidden='true'> </div>";
-                    document.getElementById("exampleModalCenter" + i).innerHTML = "<div class='modal-dialog modal-dialog-centered' role='document'> <div class='modal-content'> <div class='modal-header'> <h3 class='modal-title' id='exampleModalLongTitle'>" + data.recipes[i].title + "</h3> <button type='button' class='close' data-dismiss='modal' aria-label='Close'> <span aria-hidden='true'>&times;</span> </button> </div> <div class='modal-body' id='popupBody'>" + "Health Score: " + data.recipes[i].healthScore + "<br /> Dairy Free: " + data.recipes[i].dairyFree + "<br /> Gluten Free: " + data.recipes[i].glutenFree + "<br /> Vegan: " + data.recipes[i].vegan + "<br /> Low Fodmap: " + data.recipes[i].lowFodmap + "</div> </div> </div>";
+                    document.getElementById("exampleModalCenter" + i).innerHTML = "<div class='modal-dialog modal-dialog-centered' role='document'> <div class='modal-content'> <div class='modal-header'> <h3 class='modal-title' id='exampleModalLongTitle'>" + data.recipes[i].title + "</h3> <button type='button' class='close' data-dismiss='modal' aria-label='Close'> <span aria-hidden='true'>&times;</span> </button> </div> <div class='modal-body' id='popupBody'>" + "Health Score: " + healthScore + "<br /> Dairy Free: " + dairyFree + "<br /> Gluten Free: " + glutenFree + "<br /> Vegan: " + vegan + "<br /> Low Fodmap: " + lowFodmap + "</div> </div> </div>";
                 }
             });
         }
