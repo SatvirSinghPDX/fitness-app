a3f85ab2c64a80c68bbacf25015063f20a8a5097
Alex <alex_llamas@icloud.com>
2020-05-26 03:19:03 -0700

    data showing on exercises UI

--- Exercises.js
+++ Exercises.js
@@ -1,35 +1,33 @@
-var ExercisesUrl = 'https://wger.de/api/v2/exercise/?page=1'
 const muscle = document.getElementById('muscle-group').textContent;
-let equipment = 1;
-// console.log(muscle);
+var ExercisesUrl = 'https://wger.de/api/v2/exercise/?language=2&category=' + muscle;
+let equipment = 7;
 
 function filterEquipment(eqp) { 
+    document.getElementById("exercise-listing").innerHTML = "";
     equipment = eqp.value; 
-    document.getElementById("TRYME").innerHTML = "";
+
+    if (equipment != 11) {
+        ExercisesUrl += '&equipment=' + equipment;
+    }
     getExercises();
 } 
 
 function getExercises() {
+    console.log(ExercisesUrl)
    fetch(ExercisesUrl)
         .then(response => response.json())
         .then(data => {
             data.results.forEach(function (obj) { 
-                console.log("searching")
-                if (obj.equipment.length == 0) {
-                    obj.equipment.push(7);
-                }
-                if (obj.name !== '' && obj.equipment == equipment && obj.language === 2 && obj.category == muscle) {
-                    console.log("muscle " + obj.category);
-                    console.log("equipment " + obj.equipment);
-                    document.getElementById("TRYME").innerHTML += "<p>" + obj.name + "</p>";
+                if (obj.name !== '' && obj.description !== '') {
+                    document.getElementById("exercise-listing").innerHTML += "<div class='exercise-item shadow-lg p-3 mb-5 rounded'>" + "<label>" + obj.name + "</label>" + "<p>" + obj.description + "</p>" + "</div>";
                 }
             }); 
             if(data.next !== null) {
                 ExercisesUrl = data.next;
-                getExercises(muscle, equipment);
+                getExercises();
             }
             else {
-                ExercisesUrl = 'https://wger.de/api/v2/exercise/?page=1';
+                ExercisesUrl = 'https://wger.de/api/v2/exercise/?language=2&category=' + muscle;
             }
             
         })
